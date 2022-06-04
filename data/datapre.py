8faa19311773b673b219edae64e5ff73394a5c5a
rolovoid <1225490622@qq.com>
2022-06-04 15:59:32 +0800

    update: rearrange file structure

--- data/datapre.py
+++ data/datapre.py
@@ -2,8 +2,10 @@
 
 import jqdatasdk as jq
 import yaml
+import os.path
 
-# We can use tushare free api part instead if we have no access to jqdatasdk
+# We can attempt to use old tushare free api part instead if we have no access to jqdatasdk, but there is only data in past three months 
+# ToDo: implement tushare methods
 # import tushare as ts
 # print(ts.get_hist_data('600847', ktype='15',start='2010-01-01',end='2022.06.01') )
 # print(ts.get_hs300s())
@@ -13,21 +15,31 @@ target = open("./config.yaml",'r',encoding='utf-8')
 config = target.read()
 data = yaml.load(config,Loader=yaml.FullLoader)
 
+# get target filename
+filename = data['stockts']['index']+'.'+data['stockts']['fre']+'.csv'
+
 # get data via jqdatasdk
-jq.auth(data['metadata']['username'],data['metadata']['password'])
-stockcodes = jq.get_index_stocks(data['stockts']['index'], date=None)
-df = jq.get_price(stockcodes, 
-                end_date=data['stockts']['enddate'], 
-                count=data['stockts']['count'], 
-                frequency=data['stockts']['fre'], 
-                fields=data['stockts']['fields'],
-                panel=False)
+if not os.path.exists('../dataset/'+filename): 
+    jq.auth(data['metadata']['username'],data['metadata']['password'])
+    stockcodes = jq.get_index_stocks(data['stockts']['index'], date=None)
+    df = jq.get_price(stockcodes, 
+                    end_date=data['stockts']['enddate'], 
+                    count=data['stockts']['count'], 
+                    frequency=data['stockts']['fre'], 
+                    fields=data['stockts']['fields'],
+                    panel=False)
 
-# save data to local directory in order to use again
-filename = data['stockts']['index']+'.'+data['stockts']['enddate']+'.'+data['stockts']['fre']+'.csv'
-df.to_csv(filename)
-# print(df.at[1,'code'])
+    # print(df.at[1,'code'])
+    # save data to local directory
+    df.to_csv('../dataset/'+filename)
 
-# group by stockcodes
-grouped = df.groupby('code')
-# print(grouped.get_group('600763.XSHG'))
+    # generate labels
+    # for example, if we want to predict the close price trend of 6.4, 
+    # then we choose data from 6.3 as dataset and relevant trend of 6.3 and 6.4 as label 
+    grouped = df.groupby('code')
+    
+
+
+# # group by stockcodes
+# grouped = df.groupby('code')
+# # print(grouped.get_group('600763.XSHG'))
